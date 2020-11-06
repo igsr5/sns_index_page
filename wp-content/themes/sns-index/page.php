55cec8428adeb18bce3349f2a8cf7f052c696628
Ichigo Sora <ichigo@web-freework.com>
2020-11-06 16:32:59 +0900

    update: ユーザー検索フォームを作成

--- wp-content/themes/sns-index/page.php
+++ wp-content/themes/sns-index/page.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
 <div class="container">
-    <h2 class="user-name">sora さん</h2>
+    <div class="row head">
+        <div class="col-sm-7">
+            <h2 class="user-name">sora さん</h2>
 
-    <?php if(!htmlspecialchars($_GET["tag"])): ?>
-    <a href="?tag=1">自分に関する投稿すべて表示</a>
-    <?php else: ?>
-    <a href="?tag=0">自分の投稿のみ表示</a>
-    <?php endif; ?>
+            <?php if (!htmlspecialchars($_GET["scope"])): ?>
+                <a href="?scope=1">自分に関する投稿すべて表示</a>
+            <?php else: ?>
+                <a href="?scope=0">自分の投稿のみ表示</a>
+            <?php endif; ?>
+        </div>
+        <div class="col-sm-5 user-search">
+            <form class="row" action="">
+                <input class="form-control col-5 offset-1" type="text" name="user_name" placeholder="username">
+                <input class="btn btn-primary col-3 ml-2" type="submit" value="検索">
+            </form>
+        </div>
+    </div>
+
+
 </div>
 <?php get_footer(); ?>
