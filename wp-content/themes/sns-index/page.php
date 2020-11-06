ac3ea60b9bad970d612024653eda4f0cef71d22c
Ichigo Sora <ichigo@web-freework.com>
2020-11-06 15:21:05 +0900

    update: 投稿内容を変更するリンクを作成

--- wp-content/themes/sns-index/page.php
+++ wp-content/themes/sns-index/page.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
+<div class="container">
+    <h2 class="user-name">sora さん</h2>
 
+    <?php if(!htmlspecialchars($_GET["tag"])): ?>
+    <a href="?tag=1">自分に関する投稿すべて表示</a>
+    <?php else: ?>
+    <a href="?tag=0">自分の投稿のみ表示</a>
+    <?php endif; ?>
+</div>
 <?php get_footer(); ?>
