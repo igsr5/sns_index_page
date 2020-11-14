b06f0387e313d2971350455861fcdfbb1db5b611
Ichigo Sora <ichigo@web-freework.com>
2020-11-14 20:47:04 +0900

    update: 検索機能を追加

---
+++ wp-content/themes/sns-index/searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" class="row" action="<?php bloginfo('url'); ?>">
+    <input class="form-control col-5 offset-1" type="text" name="s" placeholder="username">
+    <input class="btn btn-primary col-3 ml-2" type="submit" value="検索">
+</form>
